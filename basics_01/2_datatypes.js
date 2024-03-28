2d768b45c2751a915eb8491158a6ec2490eae414
sonalisingh21 <130287457+sonalisingh21@users.noreply.github.com>
2024-03-28 05:34:19 +0000

    Datatypes in javascript

---
+++ basics_01/2_datatypes.js
@@ -0,0 +1,24 @@
+//datatype;
+
+//1. number
+let age = 33
+
+//2. strings
+let name = "sonali"
+
+//3. boolean values
+let isLoggedIn = true
+
+//4. null 
+let bankBalance = null
+
+//5. undefined
+let city;
+
+//6. symbol - used to find uniqueness in react components;
+
+//7. object - used a lot ; null is also an object
+
+//you can also check a type of datatype using typeof keyword
+console.log(typeof isLoggedIn);
+console.log(typeof null);
